8b7e221b15dd6e5cbf73e40a2a84ce0ec5b64ef2
oonjib <backdev.oonjib@gmail.com>
2024-02-10 15:34:01 +0330

    fix https

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,5 +1,7 @@
 import io from "socket.io-client";
 
-const socket = io(`https://${process.env.EXPO_PUBLIC_BASE_URL}:4000`);
+const socket = io(`https://${process.env.EXPO_PUBLIC_BASE_URL}:4000`, {
+  secure: true,
+});
 
 export default socket;
