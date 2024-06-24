15675aab55b40091208df9856140827b54d8e0b3
oonjib <backdev.oonjib@gmail.com>
2024-06-24 17:03:10 +0330

    fix socket

--- lib/Instance.ts
+++ lib/Instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.EXPO_PUBLIC_BASE_URL}`,
+  baseURL: `${process.env.EXPO_PUBLIC_BASE_URL}/api`,
 });
 
 axiosInstance.interceptors.response.use(
