f31f465d985b4e0e5fbf9f5bb7f18e879b777e92
oonjib <backdev.oonjib@gmail.com>
2024-07-04 18:47:04 +0330

    fix call

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,5 @@
 import io from "socket.io-client";
 
-const socket = io(`${process.env.EXPO_PUBLIC_BASE_URL}`, {
-  transports: ["websocket"],
-});
+const socket = io(`${process.env.EXPO_PUBLIC_BASE_URL}`, {});
 
 export default socket;
